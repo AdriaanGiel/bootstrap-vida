c74758298d5dee67d7daffa258e6e3dc0103acd1
Dion <0949000@hr.nl>
2020-04-13 22:04:12 +0200

    persona's aangepast margins

--- src/js/sections/research-section.vue
+++ src/js/sections/research-section.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="research" class="background-secondary-blue">
         <title-card title="Onderzoek" />
-        <div class="container-fluid px-0 section position-relative">
-            <div class="container py-5 h-100 d-flex flex-column justify-content-center align-items-center">
+        <div class="container-fluid px-0  position-relative">
+            <div class="container pt-5 pb-0 h-60 d-flex flex-column justify-content-center align-items-center">
                 <div class="row mb-5">
                         <div class="col-md-6">
                             <p>Voor ons onderzoek hebben we gesproken met de
@@ -32,58 +32,40 @@
                             </p>
                         </div>
                     </div>
-                <div class="row w-100">
-                    <div class="col-md-12 px-0">
-                        <div class="card w-100 background-secondary-blue z-top r_uitleg">
-                            <div class="card-body d-flex flex-column justify-content-center align-items-center bg-gray-300">
-                                <div class="row w-100">
-                                    <div class="col-md-4 d-flex flex-row justify-center align-items-center">
-                                        <icon src="img/dollar-sign.png"/>
-                                        <div>
-                                            <p class="r_title ml-2 mt-3 mb-1">Betaalbaar</p>
-                                            <p class="r_text ml-2" >Betaalbaarheid, het zal uiteindelijk 100
-                                                euro moeten gaan kosten voor een student.</p>
-                                        </div>
-                                    </div>
+               </div>
+        </div>
 
-                                    <div class="col-md-4 d-flex flex-row">
-                                        <icon src="img/privacy.png" />
-                                        <div>
-                                            <p class="r_title ml-2 mt-3 mb-1">Privacy</p>
-                                            <p class="r_text ml-2">Er is data gekoppeld aan een robot. Deze data
-                                                zal niet vrij gegeven mogen worden.</p>
+            <div class="container-fluid  px-0 position-relative">
+                <div class="container pt-5 pb-0 h-60 d-flex flex-column justify-content-center align-items-center">
+                    <div class="row w-100">
+                        <div class="col-md-12 px-0">
+                            <div class="card w-100 background-secondary-blue z-top ">
+                                <div class="card-body d-flex flex-column justify-content-center align-items-center bg-gray-300">
+                                    <div class="row w-100">
+                                        <div class="col-md-4 d-flex flex-row justify-center align-items-center mb-2">
+                                            <icon src="img/dollar-sign.png"/>
+                                            <div>
+                                                <p class="r_title ml-2 mt-3 mb-1">Betaalbaar</p>
+                                                <p class="r_text ml-2" >Betaalbaarheid, het zal uiteindelijk 100
+                                                    euro moeten gaan kosten voor een student.</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-4 d-flex flex-row">
-                                        <icon src="img/chatbot.png" size="4"/>
-                                        <div>
-                                            <p class="r_title ml-2 mt-3 mb-1">Interactie</p>
-                                            <p class="r_text ml-2">Het prototype zal de enige interactie bieden,
-                                                tussen de gebruiker en zijn/haar school.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row w-100">
-                                    <div class="col-md-4 d-flex flex-row mb-2">
-                                        <icon src="img/team.png"/>
-                                        <div>
-                                            <p class="r_title ml-2 mt-3 mb-1">Personalisatie</p>
-                                            <p class="r_text ml-2">Er kan een band worden ontstaan wanneer de robot gepersonaliseerd is.</p>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4 d-flex flex-row mb-2">
-                                        <icon src="img/robot-2.png"/>
-                                        <div>
-                                            <p class="r_title ml-2 mt-3 mb-1">Vorm</p>
-                                            <p class="r_text ml-2">Vorm en uiterlijk is een belangrijk aspect voor iemand die er naar moet kijken.</p>
-                                        </div>
-                                    </div>
 
-                                    <div class="col-md-4 d-flex flex-row mb-2">
-                                        <icon src="img/fun.png" size="4"/>
-                                        <div>
-                                            <p class="r_title ml-2 mt-3 mb-1">Leuk</p>
-                                            <p class="r_text ml-2">Het moet leuk blijven, de functies zijn momenteel niet heel spannend.</p>
+                                        <div class="col-md-4 d-flex flex-row mb-2">
+                                            <icon src="img/privacy.png" />
+                                            <div>
+                                                <p class="r_title ml-2 mt-3 mb-1">Privacy</p>
+                                                <p class="r_text ml-2">Er is data gekoppeld aan een robot. Deze data
+                                                    zal niet vrij gegeven mogen worden.</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 d-flex flex-row mb-2">
+                                            <icon src="img/chatbot.png" size="4"/>
+                                            <div>
+                                                <p class="r_title ml-2 mt-3 mb-1">Interactie</p>
+                                                <p class="r_text ml-2">Het prototype zal de enige interactie bieden,
+                                                    tussen de gebruiker en zijn/haar school.</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -92,12 +74,47 @@
                     </div>
                 </div>
             </div>
-            <div class="container-fluid position-absolute top background-primary-blue bottom-blue cont1 "></div>
 
-        </div>
-        <div class="container-fluid px-0  position-relative">
+            <div class="container-fluid background-primary-blue px-0  pb-0">
+                <div class="container  pb-0  d-flex flex-column justify-content-center align-items-center">
+                    <div class="row w-100">
+                        <div class="col-md-12 px-0">
+                            <div class="card w-100 background-secondary-blue z-top ">
+                                <div class="card-body d-flex flex-column justify-content-center align-items-center bg-gray-300">
+                                    <div class="row w-100">
+                                        <div class="col-md-4 d-flex flex-row mb-2">
+                                            <icon src="img/team.png"/>
+                                            <div>
+                                                <p class="r_title ml-2 mt-3 mb-1">Personalisatie</p>
+                                                <p class="r_text ml-2">Er kan een band worden ontstaan wanneer de robot gepersonaliseerd is.</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 d-flex flex-row mb-2">
+                                            <icon src="img/robot-2.png"/>
+                                            <div>
+                                                <p class="r_title ml-2 mt-3 mb-1">Vorm</p>
+                                                <p class="r_text ml-2">Vorm en uiterlijk is een belangrijk aspect voor iemand die er naar moet kijken.</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4 d-flex flex-row mb-2">
+                                            <icon src="img/fun.png" size="4"/>
+                                            <div>
+                                                <p class="r_title ml-2 mt-3 mb-1">Leuk</p>
+                                                <p class="r_text ml-2">Het moet leuk blijven, de functies zijn momenteel niet heel spannend.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div class="container-fluid background-primary-blue px-0  position-relative">
             <div class="container h-100 persona-eigenschappen">
-                <div class="row w-100 mr-0 ">
+                <div class="row w-100 sm:ml-0  ">
                         <div class="card w-100  z-top persona-box">
                             <div class="card-body p-0 d-flex  justify-content-center align-items-center">
                                 <div class="row w-100">
@@ -118,8 +135,7 @@
                                             </p>
                                         </div>
                                     </div>
-
-                                    <div class="col-md-6 px-3 p-4 d-flex flex-column background-secondary-blue align-items-center flex-lg-row ">
+                                    <div class="col-md-6 px-3 p-4 d-flex flex-column align-items-center flex-lg-row ">
                                         <div class="persona-img mr-5">
                                             <img src="img/3.png" style="width: 8em; height: 8em; border-radius:50%;">
                                         </div>
@@ -141,47 +157,47 @@
                         </div>
                 </div>
             </div>
-            <div class="container-fluid position-absolute top background-primary-blue bottom-blue cont1 "></div>
+<!--            <div class="container-fluid position-absolute  background-primary-blue bottom-blue  "></div>-->
         </div>
 
-        <div class="container py-5 h-100 d-flex flex-column justify-content-center align-items-center ">
-        <div class="row w-100">
-            <div class="col-md-12 px-0">
-                <div class="card w-100  z-top r_uitleg">
-                    <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                        <div class="row w-100">
-                            <div class="col-md-4 d-flex flex-row">
-                                <icon src="img/respect.png" size="3.5" class=""/>
-                                <div>
-                                    <p class="r_title ml-2 mt-3 mb-2">Begrip</p>
-                                    <p class="r_text ml-2">Werken van een afstand is lastig en voor beide doelgroepen is het belangrijk dat ze open staan voor elkaar.
-                                        De robot moet dit kunnen communiceren.</p>
+        <div class="container-fluid background-primary-blue  position-relative pt-10">
+            <div class="container pl-2 py-5 h-100 d-flex flex-column justify-content-center align-items-center position-relative ">
+             <div class="row w-100 lg:ml-12  lg:pl-16 sm:ml-0">
+                <div class="col-md-11 px-0">
+                    <div class="card w-100  z-top r_uitleg">
+                        <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                            <div class="row w-100">
+                                <div class="col-md-12 d-flex flex-row pb-4">
+                                    <icon src="img/respect.png" size="3.5" class=""/>
+                                    <div class="ml-16">
+                                        <p class="r_title ml-2 mt-3 mb-2">Begrip</p>
+                                        <p class=" ml-2">Werken van een afstand is lastig en voor beide doelgroepen is het belangrijk dat ze open staan voor elkaar.
+                                            De robot moet dit kunnen communiceren.</p>
+                                    </div>
                                 </div>
-                            </div>
-
-                            <div class="col-md-4 d-flex flex-row">
-                                <icon src="img/target.png" size="3.5"/>
-                                <div>
-                                    <p class="r_title  ml-2 mt-3 mb-2">Emotie</p>
-                                    <p class="r_text ml-2">De gebruiker voelt zich een minder deel van het team door de afstand
-                                        en zal het daarom moeilijker vinden om aan te geven of ze het ergens wel of niet mee eens zijn.</p>
+                                <div class="col-md-10 d-flex flex-row pb-4">
+                                    <icon src="img/target.png" size="3.5"/>
+                                    <div class="ml-16">
+                                        <p class="r_title  ml-2 mt-3 mb-2">Emotie</p>
+                                        <p class=" ml-2">De gebruiker voelt zich een minder deel van het team door de afstand
+                                            en zal het daarom moeilijker vinden om aan te geven of ze het ergens wel of niet mee eens zijn.</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4 d-flex flex-row">
-                                <icon src="img/team.png" size="3.5"/>
-                                <div>
-                                    <p class="r_title ml-2 mt-3 mb-2">Samenwerking</p>
-                                    <p class="r_text ml-2 ">Het communicatiemiddel zal ervoor zorgen dat de communicatie door
-                                        kan gaan en niet voor onderbrekingen zorgen.</p>
+                                <div class="col-md-10 d-flex flex-row pb-4">
+                                    <icon src="img/team.png" size="3.5"/>
+                                    <div class="ml-16">
+                                        <p class="r_title ml-2 mt-3 mb-2">Samenwerking</p>
+                                        <p class="ml-2 ">Het communicatiemiddel zal ervoor zorgen dat de communicatie door
+                                            kan gaan en niet voor onderbrekingen zorgen.</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+              </div>
+             </div>
         </div>
-      </div>
-        <div class="container-fluid position-absolute top background-primary-blue bottom-blue cont1 "></div>
 </div>
 
 </template>
@@ -197,13 +213,10 @@
 </script>
 
 <style scoped lang="stylus">
-.persona-box{
-    margin-top: 10em
-}
 
-
-
-
+    .persona-box{
+        top: 2em;
+    }
     .r_title{
         color:#36a9e1;
         font-weight: bold;
@@ -217,16 +230,11 @@
         font-weight: bold;
     }
 
-.persona-eigenschappen{
-    z-index: 999999;
-    position: relative;
-    bottom: 140px;
-    left: 15px;
-}
     .r_uitleg{
-        z-index: 999999;
-        position: relative;
-        top: 25px;
+        right:15px;
     }
 
+    .test_1{
+        height: 400px;
+    }
 </style>
